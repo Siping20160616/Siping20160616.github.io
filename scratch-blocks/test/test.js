63956e8ce12e2e27c4e72ff122fde52dbaf21d5b
hoozukiyama <hoozukiyama@shinke.com>
2016-06-17 22:05:38 +0900

    Update test.js

--- scratch-blocks/test/test.js
+++ scratch-blocks/test/test.js
@@ -1,29 +1,29 @@
 (function(ext){
-
   // shutdown
   ext._shutdown=function(){};
-
   // status
   ext._getStatus=function(){
     return {status:2,msg:'Ready'};
   };
-
   // connect block
   ext.connect=function(str){
+    
   };
-
+  ext.script = function(str) {
+    var script;
+    eval(str);
+    return script();
+  };
+  
   // descriptor block
   var descriptor={
       blocks: [
-          [' ','connect %m.tx','connect','connect'],
+          [' ','connect %s','connect','connect'],
+          ['r','script %s','scripts','script=function(){return ¥"hello world¥";}']
       ],
-      menus:{
-        tx: ['<', '>']
-      },
+      menus:{},
       url: 'https://Siping20160616.github.io/scratch-blocks/test'
   };
-
   // Register ScratchExtension
   ScratchExtensions.register('Test Extension',descriptor,ext);
-
 })({});
